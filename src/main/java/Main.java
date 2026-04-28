6f4f5268b6ce3f7eb4dd2fd55cf258032ce0fedf
ParkJunYong <dyden119@gmail.com>
2026-04-28 12:43:05 +0900

    할 일 추가, 조회

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> todos = new ArrayList<>();
         while (true) {
             System.out.println("==== Todo App ====");
             System.out.println("1. 할 일 추가");
@@ -11,9 +14,16 @@ public class Main {
             System.out.print("메뉴 선택: ");
             String input = scanner.nextLine();
             if (input.equals("1")) {
-                System.out.println("할 일 추가 기능은 아직 준비 중입니다.");
+                System.out.print("할 일 입력: ");
+                String todo = scanner.nextLine();
+                todos.add(todo);
+                System.out.println("할 일이 추가되었습니다.");
             }else if (input.equals("2")) {
-                System.out.println("목록 보기 기능은 아직 준비 중입니다.");
+                int index = 0;
+                for(String todo : todos) {
+                    System.out.println(index + ". " + todo);
+                    index++;
+                }
             }else if(input.equals("0")){
                 System.out.println("프로그램을 종료합니다.");
                 break;
